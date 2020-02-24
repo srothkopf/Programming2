fc1c27f80b39c87ca85975940385c223b1eb2a10
starrothkopf <srothkopf@fwparker.org>
2020-02-24 09:54:01 -0600

    Merge branch 'master' of https://github.com/fwparkercode/P2_SP20
    # Conflicts:
    #	Notes/NotesB/03_loops.py

--- Labs/Alice Spellcheck/ch15Lab.py
+++ Labs/Alice Spellcheck/ch15Lab.py
@@ -11,7 +11,35 @@ import re
 def split_line(line):
     # This function takes in a line of text and returns
     # a list of words in the line.
-    return re.findall('[A-Za-z]+(?:\'[A-Za-z]+)?', line)
+    return re.findall('[A-Za-z]+(?:\'[A-Za-z]+)?', line) # STEP 1
+
+file = open("../Alice Spellcheck/dictionary.txt") # STEP 2
+with open("../Alice Spellcheck/dictionary.txt") as f:
+    dictionary_words = [x.strip().upper() for x in f]
+file.close() # STEP 3
+
+print("--- Linear Search ---") # STEP 4
+
+file = open("../Alice Spellcheck/AliceInWonderLand200.txt") # STEP 5
+line_number = 0
+
+def linear_search(key, my_list):
+    i = 0
+    while i < (len(my_list) - 1) and key.upper() != my_list[i]:  # STEP 10
+        i += 1
+    if i == len(my_list):
+        print(key, "not found")
+
+
+for line in file: # STEP 7
+    line = line.strip().upper()
+    line_number += 1
+    words = split_line(line) # STEP 8
+    for word in words:  # STEP 9
+        linear_search(word, words)
+
+file.close() # STEP 12
+
 
 
 
