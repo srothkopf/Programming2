761a16068c43483ce5754dbbe46c660cc764f7e2
starrothkopf <srothkopf@fwparker.org>
2020-03-30 11:22:39 -0500

    Merge branch 'master' of https://github.com/fwparkercode/P2_SP20
    # Conflicts:
    #	Labs/CTA Ridership/cta_rider_lab.py

--- Notes/NotesB/temp_10_plotting_scatter.py
+++ Notes/NotesB/temp_10_plotting_scatter.py
@@ -2,6 +2,61 @@ import csv
 import matplotlib.pyplot as plt
 import numpy as np
 
-with open("") as f:
-    reader = csv.reader(f, delimeter='\t')
-    data = list()
+with open("World firearms murders and ownership - Sheet 1.tsv") as f:
+    reader = csv.reader(f, delimiter='\t')
+    data = list(reader)
+
+print(data)
+
+print()
+header = data.pop(0)
+print(header)
+
+# make a scatter of firearms_per_100k vs homicides_100k
+
+homicide_100k = []
+firearms_100 = []
+countries = []
+similar = ["Canada", "England and Wales", "Japan", "South Korea", "France","Germany","Switzerland","Ireland","China",
+           "Singapore","Hungary","Isreal","Taiwan","Spain","Belgium","Finland","Netherlands", "United States"]
+
+data = [x for x in data if x[0] in similar]
+
+for country in data:
+    try:
+        homicides = float(country[5])
+        firearms = float(country[-2])
+        name = country[0]
+        homicide_100k.append(homicides)
+        firearms_100.append(firearms)
+        countries.append(name)
+    except:
+        print(country[0], "data is incomplete.")
+
+print(countries)
+
+plt.figure("Homicides per Firearm Worldwide", figsize=(8,6)) # add figsize
+
+plt.scatter(firearms_100, homicide_100k)
+plt.ylabel("Homicides (per 100k population)")
+plt.xlabel("Firearms (per 100 people)")
+plt.title("Homicides vs. Gun Ownership by Country")
+
+plt.annotate("MY TEXT", xy=(50, 50))
+
+for i in range(len(countries)):
+    plt.annotate(countries[i], xy=(firearms_100[i], homicide_100k[i]))
+
+# best fit line
+p = np.polyfit(firearms_100, homicide_100k, 1) # x, y, order (linear is 1st order)
+print(p)
+
+x = [x for x in range(100)]
+y = [p[0] * y + p[1] for y in x] # linear 1st order
+#y = [p[0] * y ** 2 + p[1] * y + p[2] for y in x] # 2nd order
+
+plt.plot(x, y)
+
+
+plt.show()
+
