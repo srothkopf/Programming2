3a27c389ebbe479f0f4ee30889a2f4cc79f15456
starrothkopf <srothkopf@fwparker.org>
2020-04-21 00:52:25 -0500

    Merge branch 'master' of https://github.com/fwparkercode/P2_SP20
    # Conflicts:
    #	Notes/NotesB/11_scraping_webpage.html

--- Labs/Scraping Lab/scraping-lab.py
+++ Labs/Scraping Lab/scraping-lab.py
@@ -8,6 +8,42 @@
 # Have fun.  Again, nothing explicit.
 
 #  print("{} {}!".format("Hello", "World"))
+from bs4 import BeautifulSoup
+import requests
+import time
+import urllib.request
+import certifi
+from selenium import webdriver
+
+twit_url = "https://twitter.com/FWProbotics"
+driver = webdriver.Chrome("/Users/starrothkopf/PycharmProjects/P2_SP20/Labs/Scraping Lab/chromedriver")
+driver.implicitly_wait(10)
+driver.get(twit_url)
+
+last_height = driver.execute_script("return document.body.scrollHeight")
+x = 0
+pages_to_scroll = 20
+
+while x < pages_to_scroll:
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    x += 1
+    time.sleep(1)
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        break
+    last_height = new_height
+
+soup = BeautifulSoup(driver.page_source, 'lxml')
+print(soup.prettify())
+
+title = soup.find("title")
+print(title.text)
+
+extra = soup.find_all(dir="auto")
+for i in extra:
+    print(i.text)
+
+
 
 
 # Weather Scraping (15pts)
@@ -25,3 +61,23 @@
 
 
 
+url = "https://weather.com/weather/tenday/l/69bedc6a5b6e977993fb3e5344e3c06d8bc36a1fb6754c3ddfb5310a3c6d6c87"
+
+page = requests.get(url)
+soup = BeautifulSoup(page.text, "html.parser")
+
+title = soup.find("title") #
+print(title.text)
+
+days = soup.find_all(headers="day")
+descriptions = soup.find_all(headers="description")
+highlow = soup.find_all(headers="hi-lo")
+rainchance = soup.find_all(headers="precip")
+windspeed = soup.find_all(headers='wind')
+
+for i in range(len(days)):
+    print("\n", days[i].text,
+          "will be", descriptions[i].text,
+          "with a high and low of", highlow[i].text,
+          "degrees. There will be a", rainchance[i].text,
+          "chance of rain with winds of", windspeed[i].text, ".")
